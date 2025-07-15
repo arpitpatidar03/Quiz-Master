046707e4bfffba8941e0aa4e0198ddc952991beb
arpitpatidar03 <arpitpatidar@8827gmail.com>
2025-07-15 20:01:17 +0530

    three

--- src/main/java/com/example/quiz_master/Bean/User.java
+++ src/main/java/com/example/quiz_master/Bean/User.java
@@ -11,6 +11,7 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int id;
 
     private String username;
